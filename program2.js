a4b5de0969ffb6088320c2d93daf5d97a7fc21b0
loken11 <mailaddress.lokendra@gmail.com>
2024-09-30 12:39:13 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,43 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-   
+    function romanToInt(s) {
+        // Map of Roman numerals and their corresponding integer values
+        const romanToValue = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+        
+        let total = 0;
+        
+        // Traverse the Roman numeral string
+        for (let i = 0; i < s.length; i++) {
+            // Get the current value and the next value
+            let currentValue = romanToValue[s[i]];
+            let nextValue = romanToValue[s[i + 1]];
+    
+            // If the current value is less than the next value, subtract current value
+            if (currentValue < nextValue) {
+                total -= currentValue;
+            } else {
+                // Otherwise, add the current value
+                total += currentValue;
+            }
+        }
+        
+        return total;
+    }
+    
+    // Example usage:
+    console.log(romanToInt("III"));      // Output: 3
+    console.log(romanToInt("LVIII"));    // Output: 58
+    console.log(romanToInt("MCMXCIV"));  // Output: 1994
+    
 
 };
 
