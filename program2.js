11aaa536905637221f446ff635fa3418828fcf77
loken11 <mailaddress.lokendra@gmail.com>
2024-09-30 12:38:45 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-   racters ('I', 'V', 'X', 'L', 'C', 'D', 'M'). It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+   
 
 };
 
